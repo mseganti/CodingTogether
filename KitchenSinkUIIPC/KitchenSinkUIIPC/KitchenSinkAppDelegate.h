971bd2bc9432ec7fa4ee7d2765b5084fb8a942c5
Michael Seganti <mseganti@yahoo.com>
2013-03-27 19:25:04 -0400

    Kitchen Sink Part 1 Lecture 18
    This is the UIImagePickerController version of the Kitchen Sink App.
    It also includes the Photo selection from the Camera or the Media
    Library

---
+++ KitchenSinkUIIPC/KitchenSinkUIIPC/KitchenSinkAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KitchenSinkAppDelegate.h
+//  KitchenSinkUIIPC
+//
+//  Created by Michael Seganti on 3/26/13.
+//  Copyright (c) 2013 Michael Seganti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KitchenSinkAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
